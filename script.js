064748df180be4e9ff4e1c6012801a4765b824c0
mariakkjdd <115078752+mariakkjdd@users.noreply.github.com>
2024-08-28 07:40:51 -0300

    d

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const caixaprincipal = document.querySelector(".caixa-principal");
 const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativa = document.querySelector(".caixa-alternativa");
